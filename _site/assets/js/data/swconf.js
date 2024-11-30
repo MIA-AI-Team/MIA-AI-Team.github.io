458ef45bf2a7da9d8b40d8f538cac118ad103a3f
ahmedyoussefg <a.goerany@gmail.com>
2024-12-01 01:47:19 +0200

    Fix Quizzes in HTML

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733002409',resources: [
+    cacheName: 'chirpy-1733010365',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
