896bdbce4ddda8b6cd6d6a62ead0f7f3e56f5799
ahmedyoussefg <a.goerany@gmail.com>
2024-12-01 13:48:23 +0200

    Add about page

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733012135',resources: [
+    cacheName: 'chirpy-1733053661',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
