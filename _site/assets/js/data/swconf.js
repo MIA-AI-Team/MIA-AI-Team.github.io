b294e52bfc62d72f9b924ab5a62bbd6219fd29cd
ahmedyoussefg <a.goerany@gmail.com>
2024-12-01 02:14:03 +0200

    Revert "Combine all script tags into one tag"
    This reverts commit c4944462c8f1361454063311c52ca3aade02212b.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733011808',resources: [
+    cacheName: 'chirpy-1733011014',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
