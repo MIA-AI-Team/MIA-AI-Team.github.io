c4944462c8f1361454063311c52ca3aade02212b
ahmedyoussefg <a.goerany@gmail.com>
2024-12-01 02:10:50 +0200

    Combine all script tags into one tag

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733011014',resources: [
+    cacheName: 'chirpy-1733011808',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
